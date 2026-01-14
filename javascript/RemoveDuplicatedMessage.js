cbab00f58528f80def69794a00dd9dcea9478c50
Adeoluwa <joelayomide35@gmail.com>
2026-01-14 23:13:55 +0100

    Implement message normalization to remove duplicates from input array

---
+++ javascript/RemoveDuplicatedMessage.js
@@ -0,0 +1,32 @@
+
+const input = [
+    {
+        id: "1",
+        content: "We have to make an impact"
+    },
+    {
+        id: "2",
+        content: "Bro i need a jobbbb"
+    },
+    {
+        id:"1", 
+        content: "Million Naira deals ooo"
+    }
+]
+let tracker = []
+function normalizeMessages(messages) {
+    if(!Array.isArray(messages)){
+     return console.log("Invalid Data type")
+    }
+    messages.forEach(item => {
+        let id = item.id
+        tracker.push(id)
+        if(id in tracker){
+           messages.pop(item)
+        }
+        
+    })
+   console.log(messages)
+}
+
+normalizeMessages(input)
