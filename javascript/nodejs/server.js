8a7d7366402d6512e0eb79c2be223c5e413ef503
Adeoluwa <joelayomide35@gmail.com>
2026-02-21 21:32:32 +0100

    Refactor server.js for improved readability and consistency in logging

--- javascript/nodejs/server.js
+++ javascript/nodejs/server.js
@@ -1,14 +1,16 @@
-const {createServer} = require('node:http')
-
-const hostname = '127.0.0.1'
-const port = 3000
+const { createServer } = require('node:http')
 
 const server = createServer((req, res) => {
+    console.log('--- Incoming Request ---')
+    console.log('Method:', req.method)
+    console.log('URL:', req.url)
+    console.log('Headers:', req.headers)
+
     res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+    res.setHeader('Content-Type', 'text/plain')
+    res.end('Hello World')
 })
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`)
+server.listen(3000, '127.0.0.1', () => {
+    console.log('Server running')
 })
