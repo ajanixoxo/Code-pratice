1271b78a1f3e1a072e10e5657a758c5b695fac7e
Adeoluwa Gbenro <joelayomide35@gmail.com>
2026-03-09 00:41:03 +0100

    Completed variables

--- dart/variables.dart
+++ dart/variables.dart
@@ -1,13 +1,40 @@
 void main() {
-  
-  int age =2;
-  int dob=4, accountBalance=20000;
+  int age = 2;
+  int dob = 4, accountBalance = 20000;
   String name = "Ade", school = "Highland College";
-  bool isGay= false;
+  bool isGay = false;
   print(age);
   print(dob);
   print(accountBalance);
   print(name);
   print(school);
   print(isGay);
+
+  // Dyanmic variable
+  dynamic x = 10;
+  print(x);
+  x = "Dart is great";
+  print(x);
+
+
+  final geek1 = "Geeks For Geeks";
+  print(geek1);
+
+
+  final String geek2 = "Geeks For Geeks Again!!";
+
+  print(geek2);
+
+  const geek1Const = "Geeks For Geeks";
+ 
+  print(geek1Const);
+
+  const geek2Const = "Geeks For Geeks Again!!";
+
+  print(geek2Const);
+
+  // Null varaible decleration
+  int? a;
+  a=null;
+  print(a);
 }
