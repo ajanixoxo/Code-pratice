1e8bd350f777f36cd0e0d3e4bbc5c42414cfc7c5
Adeoluwa Gbenro <joelayomide35@gmail.com>
2026-03-08 22:13:25 +0100

    Add variables.dart to demonstrate variable declaration and printing in Dart

---
+++ dart/variables.dart
@@ -0,0 +1,13 @@
+void main() {
+  
+  int age =2;
+  int dob=4, accountBalance=20000;
+  String name = "Ade", school = "Highland College";
+  bool isGay= false;
+  print(age);
+  print(dob);
+  print(accountBalance);
+  print(name);
+  print(school);
+  print(isGay);
+}
